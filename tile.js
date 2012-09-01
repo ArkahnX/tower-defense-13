229a6dc20f888b52591ce630771f4b225c05c145
Christopher Hall <Biomech.ArkahnX@gmail.com>
2012-08-31 23:41:13 -0700

    removed old code and added new tile
    unnecessary images removed. build.js updated. Water-like tile added.

--- tile.js
+++ tile.js
@@ -35,6 +35,8 @@ function makeSprites() {
 				[2, 1, 2]
 			]
 		], 32));
+	}
+	for (var i = 0; i < 3; i++) {
 		tiles.push(makeTile([
 			[32 * 32, [0, 0],
 				[1, 1, 1]
@@ -44,68 +46,14 @@ function makeSprites() {
 			]
 		], 32));
 	}
-}
-
-function spriteTest() {
-	var y = 0;
-	var x = 0;
-	for (var i = 0; i < tiles.length; i++) {
-		if ((x * 32) + 32 > canvas.width) {
-			x = 0;
-			y++;
-		}
-		context.drawImage(tiles[i], x * 32, y * 32);
-		x++;
+	for (var i = 0; i < 2; i++) {
+		tiles.push(makeTile([
+			[32 * 32, [100, 200],
+				[1.5, 1.5, 1]
+			],
+			[600, [100, 200],
+				[1.5, 1.5, 1]
+			]
+		], 32));
 	}
-}
-
-
-function oldBuildTiles() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	// grass
-	for (var i = 0; i < 32; i++) {
-		for (var j = 0; j < 32; j++) {
-			var num = random(175, 225);
-			context.fillStyle = "rgb(" + Math.floor(num / 1.5) + "," + num + "," + Math.floor(num / 2) + ")";
-			context.fillRect(i, j, 1, 1);
-		}
-	}
-	// dirt
-	for (var i = 0; i < 100; i++) {
-		var x = random(0, 31);
-		var y = random(0, 31);
-		var num = random(200, 245);
-		// 8B5742
-		context.fillStyle = "rgb(" + Math.floor(num) + "," + Math.floor(num / 1.3) + "," + Math.floor(num / 1.5) + ")";
-		context.fillRect(x, y, 1, 1);
-	}
-	// dark grass
-	for (var i = 0; i < 100; i++) {
-		var x = random(0, 31);
-		var y = random(0, 31);
-		var num = random(125, 175);
-		// 8B5742
-		context.fillStyle = "rgb(" + Math.floor(num / 2) + "," + num + "," + Math.floor(num / 2) + ")";
-		context.fillRect(x, y, 1, 1);
-	}
-	tiles.push(canvas.toDataURL());
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	// road
-	for (var i = 0; i < 32; i++) {
-		for (var j = 0; j < 32; j++) {
-			var num = random(0, 0);
-			context.fillStyle = "rgb(" + Math.floor(num / 1) + "," + num + "," + Math.floor(num / 1) + ")";
-			context.fillRect(i, j, 1, 1);
-		}
-	}
-	// moar road
-	for (var i = 0; i < 600; i++) {
-		var x = random(0, 31);
-		var y = random(0, 31);
-		var num = random(0, 50);
-		// 8B5742
-		context.fillStyle = "rgb(" + Math.floor(num) + "," + Math.floor(num / 1) + "," + Math.floor(num / 1) + ")";
-		context.fillRect(x, y, 1, 1);
-	}
-	tiles.push(canvas.toDataURL());
 }
