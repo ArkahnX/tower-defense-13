2ec7ac2bc25bbeb4f49fd272b40b322cc83e8c5b
Christopher Hall <Biomech.ArkahnX@gmail.com>
2012-09-02 13:53:33 -0700

    fixed enemy spawning.
    enemies spawn in unique locations.

--- source/js/enemy.js
+++ source/js/enemy.js
@@ -1,31 +1,44 @@
 function makeEnemy() {
-	enemyData.push(makefoes("Scoot", "normal", "FF0000", 12, 13, 125));
-	enemyData.push(makefoes("Solly", "normal", "0FF000", 18, 8, 200));
-	enemyData.push(makefoes("Pyro", "normal", "00FF00", 16, 10, 175));
-	enemyData.push(makefoes("Demo", "normal", "000FF0", 16, 9, 125));
-	enemyData.push(makefoes("Hoovey", "normal", "0000FF", 20, 7, 300));
-	enemyData.push(makefoes("Engie", "normal", "FFFF00", 16, 10, 125));
-	enemyData.push(makefoes("Medic", "normal", "0FFFF0", 14, 11, 150));
-	enemyData.push(makefoes("Sniper", "normal", "00FFFF", 12, 10, 125));
-	enemyData.push(makefoes("Spah", "normal", "FFFFFF", 10, 10, 125));
+	var used = [];
+	enemyData.push(makefoes("Scoot", "normal", "FF0000", 12, 13, 125, used));
+	enemyData.push(makefoes("Solly", "normal", "0FF000", 18, 8, 200, used));
+	enemyData.push(makefoes("Pyro", "normal", "00FF00", 16, 10, 175, used));
+	enemyData.push(makefoes("Demo", "normal", "000FF0", 16, 9, 125, used));
+	enemyData.push(makefoes("Hoovey", "normal", "0000FF", 20, 7, 300, used));
+	enemyData.push(makefoes("Engie", "normal", "FFFF00", 16, 10, 125, used));
+	enemyData.push(makefoes("Medic", "normal", "0FFFF0", 14, 11, 150, used));
+	enemyData.push(makefoes("Sniper", "normal", "00FFFF", 12, 10, 125, used));
+	enemyData.push(makefoes("Spah", "normal", "FFFFFF", 10, 10, 125, used));
 }
 
-function makefoes(name, type, color, size, speed, health) {
-	var side = Math.ceil(4 * Math.random());
+function uniqueSpawn(x, y, used) {
+	for (var i = 0; i < used.length; i++) {
+		if (used[i][0] === x && used[i][1] === y) {
+			return false
+		}
+	}
+	return true;
+}
+
+function makefoes(name, type, color, size, speed, health, used) {
+	var side = random(0, 3)
+	// 0-3 top-left clockwise
 	var x = 0;
 	var y = 0;
-	if (side === 1 || side === 3) {
-		x = Math.floor(Math.random() * 32 * 19.5);
-		if (side === 3) {
-			y = 640 - size;
+	do {
+		if (side === 0 || side === 2) {
+			x = random(0, canvasWidth - 1);
+			if (side === 2) {
+				y = canvasWidth - 1;
+			}
+		} else if (side === 1 || side === 3) {
+			y = random(0, canvasHeight - 1);
+			if (side === 1) {
+				x = canvasHeight - 1;
+			}
 		}
-	}
-	if (side === 2 || side === 4) {
-		y = Math.floor(Math.random() * 32 * 19.5);
-		if (side === 2) {
-			x = 640 - size;
-		}
-	}
+	} while (!uniqueSpawn(x, y, used));
+	used.push([x, y]);
 	return {
 		name: name,
 		type: type,
@@ -38,24 +51,23 @@ function makefoes(name, type, color, size, speed, health) {
 		path: getpath(x, y)
 	}
 }
-function getpath(x, y) {
+
+function getpath(x, y, xEnd, yEnd) {
 	// FIRST, gets the tile of the enemy in question
-	tilex = Math.round(x/32);
-	tiley = Math.round(y/32);
 	// SECOND, gets the path it must take
-	move = astar.search(map,map[tilex][tiley],map[10][10]);
-	return move
+	return astar.search(map, map[y][x], map[10][10]);
 }
+
 function enemies() {
 	for (i = 0; i < enemyData.length; i++) {
-		mook = enemyData[i];
+		var mook = enemyData[i];
 		context.beginPath();
 		context.fillStyle = mook.color;
-		context.fillRect(mook.x, mook.y, mook.size, mook.size);
+		context.fillRect(mook.x * tileSize + (tileSize / 2) - (mook.size / 2), mook.y * tileSize + (tileSize / 2) - (mook.size / 2), mook.size, mook.size);
 		context.fill();
 	}
 	// Movement function for these guys
-	/*for (i = 0; i < enemyData.length; i++) {
+/*for (i = 0; i < enemyData.length; i++) {
 		// FIRST, gets the tile of the enemy in question
 		mook - enemyData[i];
 		tilex = Math.round(mook.x/32)
