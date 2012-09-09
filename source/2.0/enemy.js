5ca29a2ec161349b2d8e1921110155de4f7736a3
Christopher Hall <Biomech.ArkahnX@gmail.com>
2012-09-09 10:49:13 -0700

    Added Comments and getPaths

--- source/2.0/enemy.js
+++ source/2.0/enemy.js
@@ -1,3 +1,8 @@
+/**
+ * Define enemies for each level of difficulty.
+ * @param  {Number} levels Number of levels of difficulty.
+ */
+
 function defineEnemies(levels) {
 	for (var i = 1; i < levels + 1; i++) {
 		var half = i / 2;
@@ -14,36 +19,78 @@ function defineEnemies(levels) {
 			pixelX: null,
 			pixelY: null,
 			pathIndex: 0,
-			color:"rgb(0,0,0)"
+			color: "rgb(0,0,0)"
 		});
 	}
 }
 
-function makeWaves(levels) {
-	var typesOfEnemies = enemies.length;
-	for (var l = 1; l < levels+1; l++) {
-		var wave = [];
-		var used = [];
-		var enemyId = 0;
-		for (var e = l; e > 0; e--) {
-			var enemyNumbers = random(floor(e / 2), floor(e * 2)) + e;
-			for (var n = 0; n < e*2; n++) {
-				var side = random(0, 3)
-				// 0-3 top-left clockwise
-				var x = 0;
-				var y = 0;
-				if (side === 0 || side === 2) {
-					x = random(0, canvasWidth - 1);
-					if (side === 2) {
-						y = canvasWidth - 1;
-					}
-				} else if (side === 1 || side === 3) {
-					y = random(0, canvasHeight - 1);
-					if (side === 1) {
-						x = canvasHeight - 1;
-					}
+/**
+ * Function to move an enemy from the wave to the map. Will only spawn where the enemy can reach the base.
+ */
+
+function spawnEnemy() {
+	if (waves[0].length) {
+		var data = waves[0].splice(0, 1)[0];
+		var testPath;
+		/**
+		 * test paths until a
+		 */
+		do {
+			var side = random(0, 3)
+			// 0-3 top-left clockwise
+			var x = 0;
+			var y = 0;
+			if (side === 0 || side === 2) {
+				x = random(0, canvasWidth - 1);
+				if (side === 2) {
+					y = canvasWidth - 1;
 				}
-				used.push([x, y]);
+			} else if (side === 1 || side === 3) {
+				y = random(0, canvasHeight - 1);
+				if (side === 1) {
+					x = canvasHeight - 1;
+				}
+			}
+			data.x = x;
+			data.y = y;
+			data.pixelX = centerSymmetrical(x, data.size);
+			data.pixelY = centerSymmetrical(y, data.size);
+			var testMap = compile();
+			testPath = getPaths([data], testMap);
+		} while (!testPath);
+		data.path = testPath;
+		data.targetX = testPath[0].x;
+		data.targetY = testPath[0].y;
+		data.pathIndex = 0;
+		onScreen.push(data);
+	} else {
+		waves.splice(0, 1);
+	}
+}
+
+/**
+ * Make enemy waves.
+ * @param  {Number} levels Number of waves to make.
+ */
+
+function makeWaves(levels) {
+	/**
+	 * Make a wave for each level
+	 */
+	for (var l = 1; l < levels + 1; l++) {
+		var wave = [];
+		var enemyId = 0;
+		/**
+		 * Start large, end small.
+		 */
+		for (var e = l; e > 0; e--) {
+			/**
+			 * Enemy numbers, based off of previous value.
+			 */
+			for (var n = 0; n < e * 2; n++) {
+				/**
+				 * EnemyId is increased after this loop is run, resulting in decreased enemies as they become more difficult.
+				 */
 				var data = cloneData(enemies[enemyId], ["x", "y", "pixelX", "pixelY"], [xModifier, yModifier, pixelXModifier, pixelYModifier], [x, y]);
 				wave.push(data);
 			}
@@ -53,38 +100,78 @@ function makeWaves(levels) {
 	}
 }
 
-function xModifier(data, attribute, add) {
-	return add[0];
+/**
+ * Function to manage cloning enemy X value. Refer to functions.js/cloneData.
+ * @param  {Object} enemy     Enemy that is being cloned.
+ * @param  {String} attribute Attribute that was targetted
+ * @param  {Array}  other     Variables added when called.
+ * @return {Number}           Changes the enemy[attribute] to the returned value.
+ */
+
+function xModifier(enemy, attribute, other) {
+	return other[0];
 }
 
-function yModifier(data, attribute, add) {
-	return add[1];
+/**
+ * Function to manage cloning enemy Y value. Refer to functions.js/cloneData.
+ * @param  {Object} enemy     Enemy that is being cloned.
+ * @param  {String} attribute Attribute that was targetted
+ * @param  {Array}  other     Variables added when called.
+ * @return {Number}           Changes the enemy[attribute] to the returned value.
+ */
+
+function yModifier(enemy, attribute, other) {
+	return other[1];
 }
 
-function pixelXModifier(data, attribute, add) {
-	return centerSymmetrical(add[0], data.size);
+/**
+ * Function to manage cloning enemy pixelX value. Refer to functions.js/cloneData.
+ * @param  {Object} enemy     Enemy that is being cloned.
+ * @param  {String} attribute Attribute that was targetted
+ * @param  {Array}  other     Variables added when called.
+ * @return {Number}           Changes the enemy[attribute] to the returned value.
+ */
+
+function pixelXModifier(enemy, attribute, other) {
+	return centerSymmetrical(other[0], enemy.size);
 }
 
-function pixelYModifier(data, attribute, add) {
-	return centerSymmetrical(add[1], data.size);
+/**
+ * Function to manage cloning enemy pixelY value. Refer to functions.js/cloneData.
+ * @param  {Object} enemy     Enemy that is being cloned.
+ * @param  {String} attribute Attribute that was targetted
+ * @param  {Array}  other     Variables added when called.
+ * @return {Number}           Changes the enemy[attribute] to the returned value.
+ */
+
+function pixelYModifier(enemy, attribute, other) {
+	return centerSymmetrical(other[1], enemy.size);
 }
+
+/**
+ * Draw each onScreen enemy.
+ */
 
 function drawEnemies() {
-	forEach(wave, function(index) {
-		if (this.health < 1) {
-			destroyEnemy(this, index);
+	forEach(onScreen, function(index) {
+		var enemy = this;
+		if (enemy.health < 1) {
+			destroyEnemy(enemy, index);
 		} else {
 			context.beginPath();
-			context.fillStyle = this.color;
-			context.fillRect(this.pixelX, this.pixelY, this.size, this.size);
+			context.fillStyle = enemy.color;
+			context.fillRect(enemy.pixelX, enemy.pixelY, enemy.size, enemy.size);
 			context.fill();
 		}
 	});
 }
-//unoptomized/incomplete
+
+/**
+ * Move enemies that are onScreen, until they reach the base.
+ */
 
 function moveEnemies() {
-	forEach(wave, function(index) {
+	forEach(onScreen, function(index) {
 		var enemy = this;
 		if (enemy.pathIndex < enemy[PATH][LENGTH]) {
 			var target = enemy[PATH][enemy.pathIndex];
@@ -125,18 +212,44 @@ function moveEnemies() {
 				enemy.pathIndex++;
 			}
 		}
-		var base = getBaseCoords()
 		if (enemy.x === base.x && enemy.y === base.y) {
 			reachBase(enemy);
-			destroyUnit(enemy[NAME])
+			destroyEnemy(enemy, index)
 		}
 	});
 }
+
+/**
+ * Remove an enemy from play. Either it's been destroyed or it reached the base.
+ * @param  {Object} enemy Enemy to remove.
+ * @param  {Number} index Index to use when splicing it from onScreen.
+ */
 
 function destroyEnemy(enemy, index) {
 	var x = enemy.pixelX;
 	var y = enemy.pixelY;
 	var modifier = enemy.size / 2;
 	makeParticles(20, 60, [2, 7], [-2, 2, -2, 2], [x, y - modifier, x + modifier, y + modifier], enemy.color);
-	wave.splice(index, 1);
+	onScreen.splice(index, 1);
+}
+
+/**
+ * Tests paths for enemies.
+ * @param  {Array} enemies  Array of enemies to find paths for.
+ * @param  {Array} map      Map to test against.
+ * @return {Boolean}        Returns path on success, false on failure.
+ */
+
+function getPaths(enemies, map) {
+	var list = [];
+	forEach(enemies, function(index) {
+		var enemy = this;
+		var compiledMap = map || compile();
+		var path = astar.search(compiledMap, compiledMap[enemy.y][enemy.x], compiledMap[base.y][base.x]);
+		list[PUSH]( !! path.length);
+	});
+	if (list.indexOf(false) > -1) {
+		return false;
+	}
+	return path;
 }
