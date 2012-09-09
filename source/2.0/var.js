37a2e025acd343d504e45ccbb0d87518b91eb930
Christopher Hall <Biomech.ArkahnX@gmail.com>
2012-09-09 13:14:40 -0700

    added variables from 1.0

--- source/2.0/var.js
+++ source/2.0/var.js
@@ -1,10 +1,17 @@
 var canvas, context;
+var animationLoop = null;
 
 var base = null;
 var timer = 0;
 var canvasWidth = 20;
 var canvasHeight = 20;
 var tileSize = 32;
+var id = 0;
+var bought = null;
+var mouse = {
+	x: 0,
+	y: 0
+};
 
 var perTileFunction = [];
 var perFrameFunction = [];
