2a636827cef9ab1b8ca9218b794455ad8b4d936c
Christopher Hall <Biomech.ArkahnX@gmail.com>
2012-09-11 23:51:58 -0700

    work on audio

---
+++ source/js/audio.js
@@ -0,0 +1,25 @@
+var audioBuffer = null;
+var context = new webkitAudioContext();
+
+function playSound(buffer) {
+	var source = context.createBufferSource(); // creates a sound source
+	source.buffer = buffer; // tell the source which sound to play
+	source.connect(context.destination); // connect the source to the context's destination (the speakers)
+	source.noteOn(0); // play the source now
+}
+
+function loadAudio(url) {
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+
+	// Decode asynchronously
+	request.onload = function() {
+		context.decodeAudioData(request.response, function(buffer) {
+			audioBuffer = buffer;
+		}, onError);
+	}
+	request.send();
+}
+
+loadAudio("shotgun.mp3");
