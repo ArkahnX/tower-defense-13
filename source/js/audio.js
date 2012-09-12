a2bb2530eb4ede5de2d83d35fa4ab1e832059be4
Christopher Hall <Biomech.ArkahnX@gmail.com>
2012-09-12 00:01:21 -0700

    added web audio api framework

--- source/js/audio.js
+++ source/js/audio.js
@@ -1,25 +1,6 @@
-var audioBuffer = null;
-var audio = new webkitAudioContext();
+var shotgunAudio = initializeNewWebAudioContext();
+shotgunAudio.loadSound('shotgun.mp3', 'shotgun');
 
-function playSound(buffer) {
-	var source = audio.createBufferSource(); // creates a sound source
-	source.buffer = buffer; // tell the source which sound to play
-	source.connect(audio.destination); // connect the source to the audio's destination (the speakers)
-	source.noteOn(0); // play the source now
+function playAudio(name) {
+	shotgunAudio.playSound(name);
 }
-
-function loadAudio(url) {
-	var request = new XMLHttpRequest();
-	request.open('GET', url, true);
-	request.responseType = 'arraybuffer';
-
-	// Decode asynchronously
-	request.onload = function() {
-		audio.decodeAudioData(request.response, function(buffer) {
-			audioBuffer = buffer;
-		});
-	}
-	request.send();
-}
-
-loadAudio("shotgun.mp3");
